22d9dbc841bf16cdb11a54299c4e0d3275387241
David Diego Gomez <diegodavid@icloud.com>
2020-03-29 02:07:32 -0300

    config

--- config/config.js
+++ config/config.js
@@ -11,12 +11,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 
 let urlDB;
 
-//if (process.env.NODE_ENV === 'dev') {
-//  urlDB = 'yourlocalMondoDB';
-//} else {
+if (process.env.NODE_ENV === 'dev') {
+  console.log("Develop");
+  urlDB = 'mongodb+srv://cafe:admin1234@cluster0-jfb6n.mongodb.net/test';
+} else {
+  console.log("production");
   //urlDB = 'mongodb+srv://cafe:admin1234@cluster0-jfb6n.mongodb.net/test';
   urlDB = `${process.env.MONGO_URI}`;
-//}
+}
 
 //nos creamos una variable global
 process.env.URLDB = urlDB;
